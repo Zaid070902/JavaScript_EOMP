c934340b3f21543f841973b02e520d4f59976629
zaid070902 <zaidflandorp4@gmail.com>
2021-08-12 16:31:23 +0200

    update

---
+++ login.js
@@ -0,0 +1,28 @@
+// let access_token = window.localStorage.getItem("jwt-token");
+function login() {
+  fetch("https://thawing-shelf-98370.herokuapp.com/auth", {
+    method: "POST",
+    body: JSON.stringify({
+      username: document.getElementById("Username").value,
+      email: document.getElementById("Email").value,
+      password: document.getElementById("Password").value,
+    }),
+    headers: {
+      "Content-type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      mystorage = window.localStorage;
+      console.log(data["access_token"]);
+      mystorage.setItem("jwt-token", data["access_token"]);
+    });
+}
+
+// function submit(event) {
+//   event.preventDefault();
+//   login();
+// }
+
+// form.addEventListener("submit", submit);
